eff2aa798b06707e7a0afa4cd2be277f739579df
simokh <m.simokhouili@gmail.com>
2021-07-04 11:36:47 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  let form = document.getElementById("create-task-form")
+
+  form.addEventListener("submit", formHandler)
+    
+})
+
+
+function formHandler(e) {
+  e.preventDefault()
+  console.log("yo what's up")
+  
+  let userInput = document.getElementById("new-task-description").value
+  let toDo = document.getElementById("tasks")
+  // let finalProduct = toDo.innerHTML += userInput
+
+  let li = document.createElement("li")
+
+  li.innerHTML = userInput
+  toDo.appendChild(li)
+  document.getElementById("new-task-description").value = ""
+
+
+
+  
+}
+
+
